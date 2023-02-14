4c63b48f263a8da8e681ada4ec64fec042ca871d
Dyey Yu En <dyeyuen17@gmail.com>
2023-02-14 18:10:54 +0800

    Adds environment configurations

---
+++ config/prod.exs
@@ -0,0 +1,9 @@
+import Config
+
+config :stonkz, :fb, 
+  verify_token: System.get_env("VERIFY_TOKEN"),
+  access_token: System.get_env("FB_ACCESS_TOKEN")
+
+config :stonkz, :services,
+  coin_gecko: StonkzWeb.Services.CoinGecko,
+  facebook: StonkzWeb.Services.Facebook
---
+++ config/test.exs
@@ -0,0 +1,9 @@
+import Config
+
+config :stonkz, :fb, 
+  verify_token: "VERIFY_TOKEN_12345",
+  access_token: "FB_ACCESS_TOKEN_12345"
+
+config :stonkz, :services,
+  coin_gecko: Stonkz.Mocks.CoinGeckoMock,
+  facebook: Stonkz.Mocks.FacebookMock
