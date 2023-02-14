a531d3869333fa1d0a302d4813b48d6bc4585850
Dyey Yu En <dyeyuen17@gmail.com>
2023-02-14 18:06:51 +0800

    Adds padding for month

--- lib/stonkz/utils/date.ex
+++ lib/stonkz/utils/date.ex
@@ -1,8 +1,10 @@
 defmodule Stonkz.Utils.Date do
   def parse(timestamp) when is_integer(timestamp) do
     {:ok, datetime} = DateTime.from_unix(timestamp, :millisecond)
+    
+    month = String.pad_leading("#{datetime.month}", 2, "0")
     day = String.pad_leading("#{datetime.day}", 2, "0")
 
-    "#{datetime.month}-#{day}-#{datetime.year}"
+    "#{month}-#{day}-#{datetime.year}"
   end
 end
